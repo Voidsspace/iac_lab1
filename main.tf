2a2cb8621fe69ab314a9150d69d42149b1e25f53
Voidsspace <brenowillian07@gmail.com>
2025-03-17 23:04:26 -0300

    Add files via upload
    IAC introdução

---
+++ main.tf
@@ -0,0 +1,38 @@
+variable "instance_type" {
+    description = "O tipo de instância a ser criada"
+    type = string
+    nullable = false
+}
+
+variable "ami" {
+  description =  "A Amazon Machine Image (AMI) a ser usada pela instância"
+  type = string
+  nullable = false
+}
+
+variable "region" {
+  description = "A região AWS onde a instancia será criada"
+  default = "us-east-2"
+  type = string
+  nullable = true
+}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "example" {
+  ami = var.ami
+  instance_type = var.instance_type
+  tags = {Name = "tarefa1-terraform"}
+}
+
+// Comandos para executar com variaveis: 
+
+// terraform init
+
+// terraform plan -var 'instance_type=t2.micro' -var 'ami=ami-0a0d9cf81c479446a' -out lab2-plan.txt
+
+// terraform apply -var 'instance_type=t2.micro' -var 'ami=ami-0a0d9cf81c479446a'
+
+//terraform destroy -var 'instance_type=t2.micro' -var 'ami=ami-0a0d9cf81c479446a'
